04023eca0cbc90f7f0b85a0ad62de0d7c246ff66
Joe Haines <hello@joehaines.co.uk>
2017-05-06 22:59:10 +0100

    Initial commit

---
+++ src/Sentimelm.elm
@@ -0,0 +1,92 @@
+module Main exposing (..)
+
+import Dict
+import Afinn
+import Html exposing (Html, input, div, text)
+import Html.Events exposing (onInput)
+import Html.Attributes exposing (value)
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { input : String
+    , score : Int
+    }
+
+
+model : Model
+model =
+    { input = ""
+    , score = 0
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Input String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Input text ->
+            { model
+                | score = scoreForWords text
+                , input = text
+            }
+
+
+scoreForWords : String -> Int
+scoreForWords text =
+    String.split " " text
+        |> List.map scoreForWord
+        |> List.sum
+
+
+scoreForWord : String -> Int
+scoreForWord word =
+    case Dict.get word Afinn.dict of
+        Just score ->
+            score
+
+        Nothing ->
+            0
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ input [ onInput Input, value model.input ] []
+        , text (scoreMessage model)
+        ]
+
+
+scoreMessage : Model -> String
+scoreMessage model =
+    if model.score > 0 then
+        "Positive"
+    else if model.score < 0 then
+        "Negative"
+    else if String.length model.input > 0 then
+        "Unknown/neutral"
+    else
+        ""
